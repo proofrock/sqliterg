cec22ad8383162f10ba6d228f7ed2f2bc241b518
Germano Rizzo <oss@germanorizzo.it>
2023-08-16 17:50:23 +0200

    **back to ARM

--- stress_ws4sqlite.sh
+++ stress_ws4sqlite.sh
@@ -7,9 +7,9 @@ rm -rf test/*.db*
 pkill -x ws4sqlite
 pkill -x sqliterg
 
-wget -q https://github.com/proofrock/ws4sqlite/releases/download/v0.15.0/ws4sqlite-v0.15.0-linux-amd64.tar.gz
-tar xzf ws4sqlite-v0.15.0-linux-amd64.tar.gz &> /dev/null
-rm -f ws4sqlite-v0.15.0-linux-amd64.tar.gz
+wget -q https://github.com/proofrock/ws4sqlite/releases/download/v0.15.0/ws4sqlite-v0.15.0-linux-arm64.tar.gz
+tar xzf ws4sqlite-v0.15.0-linux-arm64.tar.gz &> /dev/null
+rm -f ws4sqlite-v0.15.0-linux-arm64.tar.gz
 
 ./ws4sqlite --db test/bubbu.db &
 
