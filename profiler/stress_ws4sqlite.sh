6a4772f7f9178c72ed37d59460944443cd573e50
Germano Rizzo <oss@germanorizzo.it>
2023-10-07 11:22:42 +0200

    Update to ws4sqlite

--- profiler/stress_ws4sqlite.sh
+++ profiler/stress_ws4sqlite.sh
@@ -11,7 +11,7 @@ rm -f ws4sqlite*
 pkill -x ws4sqlite
 pkill -x sqliterg
 
-wget -q https://github.com/proofrock/ws4sqlite/releases/download/v0.15.0/ws4sqlite-v0.15.0-linux-amd64.tar.gz
+wget -q https://github.com/proofrock/ws4sqlite/releases/download/v0.15.1/ws4sqlite-v0.15.1-linux-amd64.tar.gz
 tar xzf ws4sqlite-v0.15.0-linux-amd64.tar.gz &> /dev/null
 
 ./ws4sqlite --db environment/test_ws4sqlite.db &
